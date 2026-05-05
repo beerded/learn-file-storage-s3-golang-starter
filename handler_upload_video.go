678df121fe0dfa5a9fbdfb123b732a1be8e8fc5e
A P <67877471+beerded@users.noreply.github.com>
2026-05-06 01:15:25 +0300

    Signed URLs implemented
    Used some crazy hacks to get a signed URL that is accessible for 10
    minutes only.

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"log"
 	"io"
 	"mime"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 	"github.com/google/uuid"
 )
+
+const defaultSignedDuration time.Duration = 10*time.Minute
 
 func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
 	// Upload limit 1GB
@@ -114,6 +121,7 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 	defer processedFile.Close()
 
 	key := fmt.Sprintf("%s/%s", viewingMode, getAssetPath(mediaType))
+
 	_, err = cfg.s3Client.PutObject(r.Context(), &s3.PutObjectInput{
 		Bucket:			aws.String(cfg.s3Bucket),
 		Key:			aws.String(key),
@@ -125,15 +133,61 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	newURL := cfg.getObjectURL(key)
+	fakeURL := fmt.Sprintf("%s,%s", cfg.s3Bucket, key)
 
 	video.UpdatedAt = time.Now()
-	video.VideoURL = &newURL
+	video.VideoURL = &fakeURL
 
 	err = cfg.db.UpdateVideo(video)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Could not update video", err)
 		return
 	}
-	respondWithJSON(w, http.StatusOK, video)
+
+	signedVideo, err := cfg.dbVideoToSignedVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not generate presigned URL", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, signedVideo)
+}
+
+func generatePresignedURL(s3Client *s3.Client,
+						  bucket, key string,
+						  expireTime time.Duration) (string, error) {
+	presignClient := s3.NewPresignClient(s3Client)
+	req, err := presignClient.PresignGetObject(context.Background(),
+    	&s3.GetObjectInput{
+			Bucket:		aws.String(bucket),
+			Key:		aws.String(key),
+		},
+		s3.WithPresignExpires(expireTime))
+	if err != nil {
+		return "", err
+	}
+	return req.URL, nil
+}
+
+func (cfg *apiConfig) dbVideoToSignedVideo(video database.Video) (database.Video, error) {
+
+	if video.VideoURL == nil {
+		return video, errors.New("video has no URL string set")
+	}
+	videoSlice := strings.Split(*video.VideoURL, ",")
+	if len(videoSlice) != 2 {
+		log.Printf("videoURL '%s' has either 0 or more than 1 commas", video.VideoURL)
+		return video, errors.New("could not split video URL")
+	}
+	bucket := videoSlice[0]
+	key := videoSlice[1]
+
+	presignedURL, err := generatePresignedURL(cfg.s3Client, bucket, key, defaultSignedDuration)
+	if err != nil {
+		return video, err
+	}
+
+	video.VideoURL = &presignedURL
+	video.UpdatedAt = time.Now()
+	return video, nil
 }
